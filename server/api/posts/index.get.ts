7f90e0bfc8995da7aa03184aa2122d8f5a216ac8
Feco <hola@fecocode.com>
2024-07-15 11:00:20 -0300

    Fix(api): Manage error in posts API

--- server/api/posts/index.get.ts
+++ server/api/posts/index.get.ts
@@ -34,72 +34,80 @@ export default defineEventHandler(async (event) => {
     publishableKey: runtimeConfig.public.CLERK_PUBLISHABLE_KEY!,
   })
 
-  const recentPostsObjects = []
+  try {
+    const recentPostsObjects = []
 
-  for (let postId of recentPostIds) {
-    const rawCatchedPost = await redis.get(`post:${postId}`)
-    let postObject = null
+    for (let postId of recentPostIds) {
+      const rawCatchedPost = await redis.get(`post:${postId}`)
+      let postObject = null
 
-    if (!rawCatchedPost) {
-      const querySnapshotOfPostSearch = await admin
-        .firestore()
-        .collection('user-posts')
-        .doc(postId)
-        .get()
+      if (!rawCatchedPost) {
+        const querySnapshotOfPostSearch = await admin
+          .firestore()
+          .collection('user-posts')
+          .doc(postId)
+          .get()
 
-      const storedPostOnDatabase = querySnapshotOfPostSearch.data()
+        const storedPostOnDatabase = querySnapshotOfPostSearch.data()
 
-      if (!storedPostOnDatabase || storedPostOnDatabase.deleted) {
-        continue
+        if (!storedPostOnDatabase || storedPostOnDatabase.deleted) {
+          continue
+        }
+
+        const parsedFoundedPost = {
+          id: storedPostOnDatabase.id,
+          text: storedPostOnDatabase.text,
+          created_at: parseFirestoreTimeStampFormatToDate(storedPostOnDatabase.created_at),
+          updated_at: parseFirestoreTimeStampFormatToDate(storedPostOnDatabase.updated_at),
+          deleted_at: parseFirestoreTimeStampFormatToDate(storedPostOnDatabase.deleted_at),
+          user_id: storedPostOnDatabase.user_id,
+          deleted: storedPostOnDatabase.deleted,
+        }
+
+        await redis.set(`post:${postId}`, JSON.stringify(parsedFoundedPost), 'EX', 60*60*24)
+        postObject = parsedFoundedPost
+      } else {
+        postObject = JSON.parse(rawCatchedPost)
       }
 
-      const parsedFoundedPost = {
-        id: storedPostOnDatabase.id,
-        text: storedPostOnDatabase.text,
-        created_at: parseFirestoreTimeStampFormatToDate(storedPostOnDatabase.created_at),
-        updated_at: parseFirestoreTimeStampFormatToDate(storedPostOnDatabase.updated_at),
-        deleted_at: parseFirestoreTimeStampFormatToDate(storedPostOnDatabase.deleted_at),
-        user_id: storedPostOnDatabase.user_id,
-        deleted: storedPostOnDatabase.deleted,
+      let author = null
+
+      const catchedAuthor = await redis.get(`author:${postObject.user_id}`)
+
+      if (!catchedAuthor) {
+        const authorClerkData = await clerk.users.getUser(postObject.user_id)
+
+        author = {
+          username: authorClerkData.username!,
+          avatar: authorClerkData.imageUrl,
+        }
+
+        await redis.set(`author:${postObject.user_id}`, JSON.stringify(author))
+      } else {
+        author = JSON.parse(catchedAuthor)
       }
 
-      await redis.set(`post:${postId}`, JSON.stringify(parsedFoundedPost), 'EX', 60*60*24)
-      postObject = parsedFoundedPost
-    } else {
-      postObject = JSON.parse(rawCatchedPost)
+      const postFormatedForFrontend: RAW_USER_POST_RESPONSE_DATA = {
+        id: postObject.id,
+        text: postObject.text,
+        created_at: postObject.created_at,
+        user_id: postObject.user_id,
+        fav_count: 0,
+        author: {
+          username: author.username!,
+          avatar: author.imageUrl,
+        }
+      }
+
+      recentPostsObjects.push(postFormatedForFrontend)
     }
 
-    let author = null
-
-    const catchedAuthor = await redis.get(`author:${postObject.user_id}`)
-
-    if (!catchedAuthor) {
-      const authorClerkData = await clerk.users.getUser(postObject.user_id)
-
-      author = {
-        username: authorClerkData.username!,
-        avatar: authorClerkData.imageUrl,
-      }
-
-      await redis.set(`author:${postObject.user_id}`, JSON.stringify(author))
-    } else {
-      author = JSON.parse(catchedAuthor)
+    return recentPostsObjects
+  } catch(error) {
+    setResponseStatus(event, 500)
+    return {
+      error
     }
-
-    const postFormatedForFrontend: RAW_USER_POST_RESPONSE_DATA = {
-      id: postObject.id,
-      text: postObject.text,
-      created_at: postObject.created_at,
-      user_id: postObject.user_id,
-      fav_count: 0,
-      author: {
-        username: author.username!,
-        avatar: author.imageUrl,
-      }
-    }
-
-    recentPostsObjects.push(postFormatedForFrontend)
   }
-
-  return recentPostsObjects
+  
 })
